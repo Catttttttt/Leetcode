af0d542178b7949a41b735c693f6ddfa8a98d628
Ryland Hu <43745623+Catttttttt@users.noreply.github.com>
2023-01-12 03:15:46 -0800

    Time: 13 ms (93.00%), Space: 13.4 MB (37.95%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -7,12 +7,12 @@ class Solution(object):
         # iteration
         if n == 0:
             return 0
-        df = [0] * (n+1)
+        df = [0] * 2
         df[0] = 0
         df[1] = 1
         for i in range(2, n + 1):
-            df[i] = df[i-1] + df[i-2]
-        return df[n]
+            df[i%2] = df[0] + df[1]
+        return df[n%2]
         
         
         ''' recursion

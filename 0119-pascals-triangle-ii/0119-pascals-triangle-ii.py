7124cb4f915a45af06360685973457deb7794cad
Ryland Hu <43745623+Catttttttt@users.noreply.github.com>
2023-01-16 00:34:10 -0500

    Time: 8 ms (98.45%), Space: 13.5 MB (14.23%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -4,9 +4,15 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
+        row = [1]
+        for _ in range(rowIndex):
+            row = [x + y for x, y in zip([0]+row, row+[0])]
+        return row
+        ''' dumb
         pt = [[1 for _ in range(j+1)] for j in range(rowIndex+1)]
         for i in range(rowIndex+1):
             for j in range(i):
                 if j != 0 and j != rowIndex:
                     pt[i][j] = pt[i-1][j-1] + pt[i-1][j]
         return pt[rowIndex]
+        '''
